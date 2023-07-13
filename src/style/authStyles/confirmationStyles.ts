c51cd31416c56bc167c45ea48feaf4ad197ee435
senyoudev <younesmeskafe2018@gmail.com>
2023-07-14 00:11:06 +0100

    confirmation screen

--- src/style/authStyles/confirmationStyles.ts
+++ src/style/authStyles/confirmationStyles.ts
@@ -1,5 +1,6 @@
 import {StyleSheet} from 'react-native';
 import authStyles from '.';
+import { getFontSize } from '../theme/globalTheme';
 
 const confirmationStyles = StyleSheet.create({
   title: {
@@ -9,8 +10,19 @@ const confirmationStyles = StyleSheet.create({
   },
   formInputs: {
     marginTop: 24,
-    marginBottom: 24,
+    marginBottom: 24, 
   },
+  confirmationContainer:{
+    width:'100%'
+  },
+  confirmationText: {
+    fontSize: getFontSize('h6'),
+    fontFamily: 'Nunito-Medium',
+    textAlign: 'center',
+    maxWidth: 240,
+    marginLeft:'auto',
+    marginRight:'auto',
+  }
 });
 
 export default confirmationStyles;
--- src/views/Auth/ConfirmationView.tsx
+++ src/views/Auth/ConfirmationView.tsx
@@ -6,17 +6,33 @@ import UseAuthViewController from "../../viewcontrollers/useAuthViewController";
 import confirmationStyles from "../../style/authStyles/confirmationStyles";
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import authStyles from "../../style/authStyles";
+import { Image } from "@rneui/themed";
+import ConfirmationInput from "../../components/confirmation/ConfirmationInput";
+
 
 const ConfirmationView = (): JSX.Element => {
     const headerBackground = require('../../assets/images/bg3.png');
-    const { largeTextColor, primaryTextColor } = getColors();
+    const image = require('../../assets/images/confirmation_letter.png');
+    const { largeTextColor, primaryTextColor,additionalInfoColor } = getColors();
 
     const { signup } = UseAuthViewController();
 
     return (
         <AuthScreen image={headerBackground} imageHeight={160}>
             <Text style={[confirmationStyles.title, { color: largeTextColor,textAlign: 'center' }]}>Confirmation</Text>
-            <View style={confirmationStyles.formInputs}>
+            <View style={[confirmationStyles.formInputs]}>
+                <View style={authStyles.header}>
+                    <Image resizeMode="contain" source={image} style={[authStyles.authPoster, { height:  70 }]} />
+                </View>
+                
+                      <View style={confirmationStyles.confirmationContainer}>
+                          <Text style={confirmationStyles.confirmationText} numberOfLines={2}>
+                            Enter the 6-digit code sent to you at example@gmail.com
+                        </Text>
+                      </View>
+
+                      <ConfirmationInput />
+                       
             </View>
             <View>
                 <CustomButton type="auth" title="Continue" action={signup} />
