2908606b7bdacee86ae9820bc3261b1444f3aa6b
Asmae <rhiati.asmae1@gmail.com>
2023-07-21 23:56:36 +0200

    add project

---
+++ src/views/ProjectView/index.tsx
@@ -0,0 +1,16 @@
+import { ScrollView, Text, TouchableOpacity, View } from "react-native"
+import MainScreen from "../../components/main/MainScreen";
+import globalStyles from "../../style";
+import { getColors } from "../../style/theme/globalTheme";
+
+const ProjectView = ({ navigation }: any) => {
+    const { largeTextColor, secondaryTextColor } = getColors();
+    
+    return (
+        <MainScreen>
+            
+
+        </MainScreen>
+    )
+}
+export default ProjectView;
